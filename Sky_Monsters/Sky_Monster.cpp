8e46fcc8e77df7fa0498e1712115509b245a0170
seans <sstclair@uvm.edu>
2023-02-13 19:27:51 -0800

    Added Sky_Monster .h and .cpp files

---
+++ Sky_Monsters/Sky_Monster.cpp
@@ -0,0 +1,5 @@
+//
+// Created by seans on 2/13/2023.
+//
+
+#include "Sky_Monster.h"
---
+++ Sky_Monsters/Sky_Monster.h
@@ -0,0 +1,8 @@
+//
+// Created by seans on 2/13/2023.
+//
+
+#ifndef MONSTERS_SKY_MONSTER_H
+#define MONSTERS_SKY_MONSTER_H
+
+#endif //MONSTERS_SKY_MONSTER_H
