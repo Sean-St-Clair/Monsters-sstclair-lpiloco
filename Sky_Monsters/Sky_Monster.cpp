9a6081c8be4331669710c7f1f897fa6797727108
seans <sstclair@uvm.edu>
2023-02-13 20:08:01 -0800

    Added Sky_Monster_Testing executable and implemented (flawed) versions of required superclass methods.

--- Sky_Monsters/Sky_Monster.cpp
+++ Sky_Monsters/Sky_Monster.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "Sky_Monster.h"
+
+Sky_Monster::Sky_Monster() {
+    number_of_wings = rand() % (MAX_NUMBER_WINGS - 1) + 2;
+}
+
+Sky_Monster::Sky_Monster(int now) {
+    set_number_of_wings(now);
+}
+
+int Sky_Monster::get_number_of_wings() const {
+    return number_of_wings;
+}
+
+void Sky_Monster::set_number_of_wings(int now) {
+    if (now < 2 || now > MAX_NUMBER_WINGS) {
+        now = MAX_NUMBER_WINGS;
+    }
+    number_of_wings = now;
+}
+
+bool Sky_Monster::growth_spurt() {
+
+    return false;
+}
